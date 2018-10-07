ab5bface9d7fef1b15617f536a6d7975088d421c
Raul Teixeira <raul.jnp.teixeira@gmail.com>
2018-10-07 18:05:43 +0100

    half arse script to remove irons mistake

--- RemovingChars.js
+++ RemovingChars.js
@@ -2,10 +2,33 @@ let path = require('path');
 appRoot = path.resolve( __dirname );
 let models = require( 'D:/TherapyTool/models/index');
 
-let charIds = [90903362, 95623437 ];
+/*charIds.forEach(async function (id) {
+    console.log("trying to remove " + id);
+    await models.loginModel.remove({ CharacterID: 90903362 }, function(err) {
+            console.log(err);
+        });
+});*/
 
-charIds.forEach(function (id) {
-    let char = models.loginModel.findOne({_id: id});
-    if(char !== undefined)
-        char.delete();
+models.loginModel.deleteOne({ CharacterID: 92643949 }, function(err) {
+    console.log(err);
+});
+
+models.loginModel.deleteOne({ CharacterID: 1501833430 }, function(err) {
+    console.log(err);
+});
+
+models.loginModel.deleteOne({ CharacterID: 1036937049 }, function(err) {
+    console.log(err);
+});
+
+models.loginModel.deleteOne({ CharacterID: 1485299290 }, function(err) {
+    console.log(err);
+});
+
+models.loginModel.deleteOne({ CharacterID: 827653312 }, function(err) {
+    console.log(err);
+});
+
+models.loginModel.deleteOne({ CharacterID: 397676440 }, function(err) {
+    console.log(err);
 });
